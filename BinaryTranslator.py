bbee83d358bfaf1e04546fc3b652e963fe7cdd7c
ndd323 <80413868+ndd323@users.noreply.github.com>
2023-03-22 14:54:15 -0500

    Binary Translator V4
    New file import system to match the instruction's example

---
+++ BinaryTranslator.py
@@ -0,0 +1,68 @@
+import sys
+
+def TranSeven(Arr, x):
+    index = 0
+    word = []
+    for i in range(int(x)): #This will iterate for length / 7 times to handle each character
+        ASCII = 0 #This will become the ASCII value of the character after processing
+        addTo = 64 #This starts at 64 since that is the value of the 1st bit in a 7-bit sequence
+        for j in range(0,7):
+            curr = Arr[index]
+            if(curr == "1"):
+                ASCII += addTo
+            addTo = addTo/2
+            index += 1
+        if(int(ASCII) == 8): # Recognizes if ASCII is 8 "Backspace" and
+            word.pop() # Removes the most recently added item
+        else:
+            character = chr(int(ASCII))
+            word.append(character)
+    answer = ""
+    for f in word:
+        answer += f
+    print(answer)
+
+def TranEight(Arr, x):
+    index = 0
+    word = []
+    for i in range(int(x)):
+        ASCII = 0
+        addTo = 128
+        for j in range(0,8):
+            curr = Arr[index]
+            if(curr == "1"):
+                ASCII += addTo
+            addTo = addTo/2
+            index += 1
+        if(int(ASCII) == 8):
+            word.pop()
+        else:
+            character = chr(int(ASCII))
+            word.append(character)
+    answer = ""
+    for f in word:
+        answer += f
+    print(answer)
+
+file = sys.stdin
+testStr = file.read()
+charArr = []
+for char in testStr:
+    if(char == '0' or char == '1'):
+        charArr.append(char)
+
+l = len(charArr)
+a = l/7
+b = l/8
+
+if(l%7 == 0 and l%8 == 0):
+    TranSeven(charArr, a)
+    TranEight(charArr, b)
+elif(l%7 == 0):
+    TranSeven(charArr, a)
+elif(l%8 == 0):
+    TranEight(charArr, b)
+else:
+    print("Invalid length of binary.")
+
+file.close()
