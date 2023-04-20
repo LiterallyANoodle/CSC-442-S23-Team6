d36ed3ffe3c5363f29f7d70c682a76cd11489a51
LiterallyANoodle <themaskedkid321@gmail.com>
2023-04-19 19:53:02 -0500

    completed client and server pair

--- CovertChat/client.py
+++ CovertChat/client.py
@@ -2,22 +2,74 @@ from socket import *
 from sys import stdout
 from time import time 
 
-ip = "localhost"
-port = 1337
+# function translates deltas back into bits 
+def read_covert(deltas):
 
-s = socket(AF_INET, SOCK_STREAM)
+    # deltas to bits
+    bits = []
+    for d in deltas:
+        if d > 0.25:
+            bits.append('1')
+        else:
+            bits.append('0')
 
-s.connect((ip, port))
+    # slice bits
+    char_bits = []
+    bits_length = range(len(bits)//7)
+    for i in bits_length:
+        char_bits.append("".join(bits[:7]))
+        bits = bits[7:]
 
-data = s.recv(4096).decode()
+    if DEBUG:
+        print(char_bits)
 
-while (data.rstrip("\n") != "EOF"):
-    stdout.write(data)
-    stdout.flush()
-    t0 = time()
+    # bit slices to ints 
+    chars = []
+    for char in char_bits:
+        chars.append(int(char, 2))
+
+    # remove any trailing 0's (ints)
+    chars = [chr(item) for item in chars if item != 0 ]
+
+    if DEBUG:
+        print(chars)
+
+    return "".join(chars)
+
+
+def main():
+
+    # connection setup 
+    ip = "138.47.99.64"
+    port = 31337
+
+    s = socket(AF_INET, SOCK_STREAM)
+
+    s.connect((ip, port))
+
     data = s.recv(4096).decode()
-    t1 = time()
-    delta = round(t1 - t0, 3)
-    stdout.write(f" {delta}\n")
+    deltas = []
 
-s.close()
+    # retrieve data and covert message
+    while (data.rstrip("\n") != "EOF"):
+        stdout.write(data)
+        stdout.flush()
+        t0 = time()
+        data = s.recv(4096).decode()
+        t1 = time()
+        delta = round(t1 - t0, 3)
+        deltas.append(delta)
+        # stdout.write(f" {delta}\n")
+
+    s.close()
+
+    if DEBUG:
+        print(deltas)
+
+    covert_message = read_covert(deltas)
+    print(covert_message)
+
+
+if __name__ == "__main__":
+    DEBUG = False
+    main()
--- CovertChat/server.py
+++ CovertChat/server.py
@@ -1,6 +1,37 @@
 from socket import *
 from time import sleep 
 
+# function creates a list of intervals to use 
+def chopping_time(covert_message):
+
+    # convert to bytes-like objects 
+    str_encode = covert_message.encode()
+    # str_bytes = []
+    deltas = []
+    for byte in str_encode:
+        if DEBUG:
+            print(bin(byte))
+        # str_bytes.append(b)
+
+        # convert bits into timings 
+        # d > 0.25 returns a 1 
+        # d <= 0.25 returns a 0 
+        byte_clean = bin(byte)[2:]
+        if DEBUG:
+            print(byte, byte_clean)
+        for bit in byte_clean:
+            # if DEBUG:
+            #     print(bit)
+            if bit == '1':
+                deltas.append(0.35)
+            else:
+                deltas.append(0.1)
+
+    if DEBUG:
+        print(deltas)
+
+    return deltas
+
 def main():
 
     # server setup 
@@ -18,13 +49,18 @@ def main():
         c, addr = s.accept()
 
         # my messages
-        message = "Some message...\n"
+        message = "This is my message. My message is very long. My message is so long, in fact, that you could probably hide another much smaller message within it.\n"
         covert_message = "Secret"
 
+        deltas = chopping_time(covert_message)
+
         # send one letter at a time
-        for i in message: 
-            c.send(i.encode())
-            sleep(0.1)
+        for i in range(len(message)): 
+            c.send(message[i].encode())
+            try:
+                sleep(deltas[i])
+            except:
+                sleep(0.1)
 
         # end and cleanup 
         c.send("EOF".encode())
@@ -34,4 +70,5 @@ def main():
     c.close()
 
 if __name__ == "__main__": 
+    DEBUG = False
     main()
